8570c9fd00172258a15149af309952d3c53e8305
imamkoo <imamkholidubaidillah16@gmail.com>
2024-03-26 22:41:36 +0700

    update : create pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+
 export default function App() {
-  return <div className="text-3xl text-red-400 text-center">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
