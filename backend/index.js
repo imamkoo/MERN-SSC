6b24acf45cbfa969f0f1915383c6abe2ae3743cb
imamkoo <imamkholidubaidillah16@gmail.com>
2024-03-29 02:25:31 +0700

    update : connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("MongoDB is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const port = 3000;
