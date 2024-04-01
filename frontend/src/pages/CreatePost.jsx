85b0aa072472f24a99e3e07ba1f7c05a9702ef84
imamkoo <imamkholidubaidillah16@gmail.com>
2024-04-01 15:24:30 +0700

    Update : show user post in the dashboard

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -146,7 +146,7 @@ export default function CreatePost() {
         <ReactQuill
           theme="snow"
           placeholder="Write something..."
-          className="h-72 mb-12"
+          className="h-72 mb-12 "
           required
           onChange={(value) => {
             setFormData({ ...formData, content: value });
