bdcb0e37ae3cbbd6f004590f3f07919504c68de6
imamkoo <imamkholidubaidillah16@gmail.com>
2024-04-02 15:38:28 +0700

    Add : comment section in the post page

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth.route.js";
+import commentRoutes from "./routes/comment.route.js";
 import postRoutes from "./routes/post.route.js";
 import userRoutes from "./routes/user.route.js";
 
@@ -28,6 +29,7 @@ app.listen(port, () => console.log(`Listening on port ${port}!`));
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import PrivateRoute from "./components/PrivateRoute";
+import ScrollToTop from "./components/ScrollToTop";
 import About from "./pages/About";
 import CreatePost from "./pages/CreatePost";
 import Dashboard from "./pages/Dashboard";
@@ -16,6 +17,7 @@ import UpdatePost from "./pages/UpdatePost";
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route element={<PrivateRoute />}>
