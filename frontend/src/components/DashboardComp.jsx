4c473c7118e2d635eaea82544fd2d3fadfa5f6c6
imamkoo <imamkholidubaidillah16@gmail.com>
2024-04-04 16:17:24 +0700

    FIx : Fixing code

--- frontend/src/components/DashboardComp.jsx
+++ frontend/src/components/DashboardComp.jsx
@@ -20,6 +20,7 @@ export default function DashboardComp() {
   const [lastMonthPosts, setLastMonthPosts] = useState(0);
   const [lastMonthComments, setLastMonthComments] = useState(0);
   const { currentUser } = useSelector((state) => state.user);
+
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -34,6 +35,7 @@ export default function DashboardComp() {
         console.log(error.message);
       }
     };
+
     const fetchPosts = async () => {
       try {
         const res = await fetch("/api/post/getposts?limit=5");
@@ -47,6 +49,7 @@ export default function DashboardComp() {
         console.log(error.message);
       }
     };
+
     const fetchComments = async () => {
       try {
         const res = await fetch("/api/comment/getcomments?limit=5");
@@ -60,12 +63,14 @@ export default function DashboardComp() {
         console.log(error.message);
       }
     };
+
     if (currentUser.isAdmin) {
       fetchUsers();
       fetchPosts();
       fetchComments();
     }
   }, [currentUser]);
+
   return (
     <div className="p-3 md:mx-auto">
       <div className="flex-wrap flex gap-4 justify-center">
