4ad953de462df8000157518fa46fbd86af6b1c72
imamkoo <imamkholidubaidillah16@gmail.com>
2024-04-04 19:48:58 +0700

    Fix : fixing code

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -149,10 +149,7 @@ export default function Search() {
         </form>
       </div>
       <div className="w-full">
-        <h1 className="text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5 ">
-          Posts results:
-        </h1>
-        <div className="p-7 flex flex-wrap gap-4">
+        <div className="p-7 flex flex-wrap gap-4 justify-center ">
           {!loading && posts.length === 0 && (
             <p className="text-xl text-gray-500">No posts found.</p>
           )}
@@ -170,7 +167,7 @@ export default function Search() {
           {showMore && (
             <button
               onClick={handleShowMore}
-              className="text-teal-500 text-lg hover:underline p-7 w-full"
+              className="text-teal-500 text-lg hover:underline p-7 w-full mx-auto"
             >
               Show More
             </button>
