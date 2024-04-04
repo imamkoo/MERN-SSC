e8bd2fcd8f724e853261c239f476ee2be12e7a17
imamkoo <imamkholidubaidillah16@gmail.com>
2024-04-04 23:03:22 +0700

    Fix index api

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 app.use("/api/chatbot", chatRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
